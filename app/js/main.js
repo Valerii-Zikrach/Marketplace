7686ad803998c798a1bf3a36c1bcd81f7b2089f7
Valerii-Zikrach <valera.zikrach@gmail.com>
2020-05-21 19:15:36 +0300

    added featured-products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
-	
+$(function () {
+	$('.item-description__rate').rateYo({
+		rating: 4.5,
+		starWidth: "17px",
+		readOnly: true,
+		ratedFill: "#ffc000"
+	});
+
+	$('.featured-products__inner').slick({
+		dots: false,
+		arrows:true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 });
