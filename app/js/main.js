f113e951431450af5f1bc84a0de05a0a0e7cbd1d
Valerii-Zikrach <valera.zikrach@gmail.com>
2020-05-22 20:16:49 +0300

    fix release-products section

--- app/js/main.js
+++ app/js/main.js
@@ -13,10 +13,25 @@ $(function () {
 		slidesToScroll: 1
 	});
 
-	$('.icon-dots-three-vertical , .head-filter__text').on('click', function () {
-		$('.head-filter__list').toggleClass('head-filter__list--active');
+
+	const menuBtn = $('.icon-dots-three-vertical, .head-filter__text'),
+		menu = $('.head-filter__list');
+	menuBtn.on('click', function () {
+		if ($('.head-filter__list').hasClass('head-filter__list--active')) {
+			$('.head-filter__list').removeClass('head-filter__list--active');
+			menu.slideUp();
+		} else {
+			$('.head-filter__list').addClass('head-filter__list--active');
+			menu.slideDown();
+		}
 	});
 
+	$(document).click(function (e) {
+		if (!menuBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
+			menu.slideUp();
+			menu.removeClass('head-filter__list--active');
+		};
+	});
 
 	var mixer = mixitup('.release-products__inner');
 
